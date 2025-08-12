a47fb414b3f59a875c8db00c8ecda2af1ce4a5d7
sthDINESH <sthapit.dnsh@gmail.com>
2025-08-12 11:04:24 +0100

    code tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  document.getElementById("answer-box").addEventListener("keydown",(event)=>{
+    if(event.key === "Enter"){
+      checkAnswer();
+    }
+  });
+  
   runGame("addition");
 });
 
@@ -21,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * loaded and after the user's answer has been processed
  */
 function runGame(gameType) {
+  document.getElementById("answer-box").value="";
+  document.getElementById("answer-box").focus();
+
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
 
