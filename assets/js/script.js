d3d669245650f06175cdbceb0139a0f3ff52bdda
sthDINESH <sthapit.dnsh@gmail.com>
2025-08-12 11:18:37 +0100

    add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,8 @@ function runGame(gameType) {
     displaySubtractQuestion(num1, num2);
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown game type:${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -82,6 +84,8 @@ function calculateCorrectAnswer() {
     return [operand1 - operand2, "subtract"];
   } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}.Aborting!`;
@@ -133,4 +137,11 @@ function displayMultiplyQuestion(operand1,operand2) {
   document.getElementById("operator").innerText = "x";
 }
 
-function displayDivisionQuestion() {}
+/**
+ * adds division question to the DOM
+ */
+function displayDivisionQuestion(operand1,operand2) {
+  document.getElementById("operand1").innerText = operand1 * operand2;
+  document.getElementById("operand2").innerText = operand2;
+  document.getElementById("operator").innerText = "/";
+}
